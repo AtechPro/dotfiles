19784191b843f5eec014b555d3488dfb4b1c955f
AtechP_5.0Ghz <amirul2278@gmail.com>
2026-09-09 09:31:55 +0800

    noctalia shell run 1st time

--- hyprland/.config/hypr/hyprland.lua
+++ hyprland/.config/hypr/hyprland.lua
@@ -60,7 +60,7 @@ local menu        = "fuzzel" -- optional; R is bound to the noctalia-shell launc
 
 -- See https://wiki.hypr.land/Configuring/Basics/Autostart/
 hl.on("hyprland.start", function()
-    hl.exec_cmd("qs -c noctalia-shell")
+    hl.exec_cmd("noctalia")
     hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
     hl.exec_cmd("sunshine")
 end)
